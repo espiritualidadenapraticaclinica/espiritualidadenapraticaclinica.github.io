92382d5ceefa89741764b1c0ecb8f11680e98a69
espiritualidadenapraticaclinica <silvia.unifesp@gmail.com>
2024-05-25 16:08:14 -0300

    Atualizando links para o novo endereço do site no GitHub Pages

--- wp-login.php
+++ wp-login.php
@@ -9,7 +9,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Acessar &lsaquo; Espiritualidade na prática clínica &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='http://espiritualidadenaclinica.com.br/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.23' type='text/css' media='all' />
+<link rel='stylesheet' href='https://espiritualidadenapraticaclinica.github.io/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.23' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -18,7 +18,7 @@
 		<div id="login">
 		<h1><a href="https://br.wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
 	
-<form name="loginform" id="loginform" action="http://espiritualidadenaclinica.com.br/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="https://espiritualidadenapraticaclinica.github.io/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Nome de usuário ou endereço de e-mail<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -30,13 +30,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Lembrar-me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acessar" />
-		<input type="hidden" name="redirect_to" value="http://espiritualidadenaclinica.com.br/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="https://espiritualidadenapraticaclinica.github.io/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-	<a href="http://espiritualidadenaclinica.com.br/wp-login.php?action=lostpassword">Perdeu a senha?</a>
+	<a href="https://espiritualidadenapraticaclinica.github.io/wp-login.php?action=lostpassword">Perdeu a senha?</a>
 </p>
 
 <script type="text/javascript">
@@ -53,7 +53,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="http://espiritualidadenaclinica.com.br/">&larr; Voltar para Espiritualidade na prática clínica</a></p>
+	<p id="backtoblog"><a href="https://espiritualidadenapraticaclinica.github.io/">&larr; Voltar para Espiritualidade na prática clínica</a></p>
 		
 	</div>
 
